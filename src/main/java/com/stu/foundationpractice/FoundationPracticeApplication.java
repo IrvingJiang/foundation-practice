6235f03e83709da6a30472b449064f3480cecc20
jiangxin <247691159@qq.com>
2024-11-04 21:52:03 +0800

    feat:init

---
+++ src/main/java/com/stu/foundationpractice/FoundationPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.stu.foundationpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoundationPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoundationPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stu/foundationpractice/FoundationPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stu.foundationpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoundationPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
